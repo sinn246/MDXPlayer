ec220080f247e4aed215a177a3fcc6055e0c835a
sinn246 <sinn246@gmail.com>
2016-10-08 21:17:33 +0900

    ファイルが追加された

---
+++ gamdx/jni/speex/speex_MDX.h
@@ -0,0 +1,16 @@
+//
+//  speex_MDX.h
+//  
+//
+//  Created by 西村 信一 on 2016/10/08.
+//
+//
+
+#ifndef speex_MDX_h
+#define speex_MDX_h
+
+//#define _USE_NEON
+// won't work... 
+#define FIXED_POINT
+
+#endif /* speex_MDX_h */
